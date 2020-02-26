fd960b930080e57eae2a1db9f9f42c88a9b4ee7a
mgarcia554 <61433970+mgarcia554@users.noreply.github.com>
2020-02-25 19:27:29 -0600

    Add files via upload

---
+++ ducklingtonInst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
+cat /root/httpd.conf>>/etc/httpd/conf/httpd.conf 
+
+mkdir -p /var/www/ducklington.cyber.net/public_html
+
+mkdir /var/www/ducklington.cyber.net/logs
+
+
+touch /var/www/ducklington.cyber.net/public_html/index.html
+
+cat index1.html>/var/www/ducklington.cyber.net/public_html/index.html
